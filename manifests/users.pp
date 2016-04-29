ed160e735bb16bbc0d39382424d853cdf38176ae
Scotty Logan <scotty@catbert.net>
2016-04-29 09:28:34 -0700

    added more SSH keys for scotty

--- manifests/users.pp
+++ manifests/users.pp
@@ -78,6 +78,20 @@ class base::users (
     tag  => [sysadmin, dev, parent],
   }
 
+  @ssh_authorized_key { 'swl-rsa':
+    user => 'scotty',
+    type => 'ssh-rsa',
+    key  => 'AAAAB3NzaC1yc2EAAAADAQABAAACAQDKnStGpMBXtYrIxganjDHZYdIuF1PklIB0mzOpmMMVklLPhB9InUnicy6KCuGCXJlyesyE8fg3wEWImfXw5mentavXvallo5sbDp2DZTBg/AAmrJzr6So/Ly5U+Z2Fy/uieAK+u0ORekCGCQX2YlZQf0zlcYnHPnwHRfVSOVz9zSdtZ9QkQbTWvI/evVYQ1VTaJ77EKKMWY0E88lLOwytM1OkQMDc8bGp03ROj8HOE7aSfxT9veWN9Kx/nr0jNEzLPF6KeQEISk7+VbyvJN3T8tREOd3B+2AVynhQ9SSnDcFsjN8HAMoW3VWbH0b/2G+6B7eIZDtqbk4sFfqb8/74xeqyoUplQ2cvLdijO1oPXfSaKKKz2tgbleZ3DOQCLA8iSk8ykZr16Nb4nU2bb5ZNqvYJIl0oyIDLJEaoEsga4UWRgmV3xMQrtK+F9DXToW+Voim2EU9AHTIir4VHnktWQtxeeuSTToHMAIhlSWMUxUBw9SqLnwA1Dim6LzeJxawhGUGBFvFi+y28+mOtiFmsl7gum47MRDr2H1Aor9grrQ5Q2LseB2+3yjZRMDUrKcsIAFBJZwkeauVjXADCwem5SPZEfNX3zXjCXirEdWb7gBoVeslVslClvLDdSje04jDALpbHzgiSdQpau42JyJw+EEO/PGAvrRkP8vN6PDxE3OQ==',
+    tag  => [sysadmin, dev, parent],
+  }
+
+  @ssh_authorized_key { 'swl-dsa':
+    user => 'scotty',
+    type => 'ssh-dss',
+    key  => 'AAAAB3NzaC1kc3MAAACBAP4mOqwuv+OPI272hYBrRCS3T6AltQKfxRSkGB7F8iY9qJjkRjwLdscKWSaWnzD+4iANdmYw+SSMwlmQ+5wxLEr4RE1onYZC20VJSsSvqiAlstUD136p0UN7kacCJR4eB+hSuT3GY7kFNb2hJo/VlgTz1YBYoWmS2XFEKAi7ubfXAAAAFQDtcG89vTIBpvPk63QdqWhQIczWIwAAAIEAreX5zEDRAaPFSQ2XiF6oKAhXapkYIn6IqBlumV0dahWPSOUvKSlR+GJ3PxYNI/Dj06CwhEsH5rqWGAe9+NMYUFiEafPjAHc4CVbUv5cLhP5nmXsEEXuz2hFXIm4qpbN+Jl+95C8yVOpfh6liVNc5TTYZ2kburWR1iBK93HyB5F8AAACBANRxUvrFuumoNCuoc5jEPwDxT94RrDoaaKDAfQQwXCHLokgyBBk+uMMLHuZo7el3pBVZ7MDknalAGi2Rye8L51K+mP28DJkkjHTPwUxWUvkgKbfj0OOc+dCVsRxn+Vo74K1dGYTpRrIvfKrhymsgf4Bt6+nqkxfWONm53v9DUpfW',
+    tag  => [sysadmin, dev, parent],
+  }
+
   @group { 'heather':
     gid => 1001,
     tag => [parent],
